960b158fbc591cf8282dac51c198c8c5372b9fe1
ashishku <ashishkujoy@gmail.com>
2024-11-23 14:41:18 +0530

    Adds jar task

--- app/src/main/kotlin/org/kapido/App.kt
+++ app/src/main/kotlin/org/kapido/App.kt
@@ -1,7 +1,19 @@
 package org.kapido
 
-import org.kapido.cli.App
+import org.kapido.cli.Parser
+import org.kapido.domain.service.FareCalculator
+import org.kapido.domain.service.Kapido
+import java.io.File
 
-fun main() {
-    App.run("./input2.txt")
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val filePath = args[0]
+            val commands = Parser().parse(File(filePath).readText().trim())
+            val kapido = Kapido(FareCalculator())
+
+            commands.map { it.execute(kapido) }.filter { it.message != null }.forEach { println(it.message) }
+        }
+    }
 }
